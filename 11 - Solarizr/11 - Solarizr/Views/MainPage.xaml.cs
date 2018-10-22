cd07dd8a5235a097600ebb854c711ac4824c8ed2
Unknown <carterasindinero@gmail.com>
2018-10-22 14:39:20 +0200

    Empezado Solarizr

---
+++ 11 - Solarizr/11 - Solarizr/Views/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using _11___Solarizr.Views;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _11___Solarizr
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //No salta a la página a menos que haya algo escrito en ambos campos
+            if(!txtUsuario.Text.Equals("") && !txtContrasena.Password.Equals(""))
+                this.Frame.Navigate(typeof(Pedidos));
+        }
+    }
+}
