5ab63d0bf0671238e4e4c9dad641c20b2bb2c6f3
Unknown <carterasindinero@gmail.com>
2018-10-23 19:28:19 +0200

    Un peaso de listview que va mal

---
+++ 11 - Solarizr/11 - Solarizr/Clases/Cita.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace App1
+{
+
+    /**
+     * Clase Cita:
+     * 
+     *      - Atributos básicos:
+     *          - IDCliente (int, consultable y modificable)
+     *          - Nombre (string, consultable y modificable)
+     *          - Apellidos (string, consultable y modificable)
+     *          - Direccion (String, consultable y modificable)
+     *          - Fecha (DateTime, consultable y modificable)
+     *          - Fotos: (Lista de imágenes, consultable y modificable)
+     *          - Comentario (String, consultable y modificable)
+     *          - Finalizada (Boolean, consultable y modificable)
+     *          
+     *      - Restricciones:
+     *          - Por ahora, no.
+     *          
+     *      - Métodos añadidos:
+     *          - :(
+     * 
+     * 
+     */
+    public class Cita
+    {
+
+        #region atributos
+        public int IDCliente { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string direccion { get; set; }
+        public DateTime fecha { get; set; }
+        public List<StorageFile> fotos { get; set; }
+        public string comentario { get; set; }
+        public bool finalizada { get; set; }
+        #endregion
+        
+    }
+}
+
+/** 
+ * 
+ *      Método asíncrono para introducir una foto a través del explorador de ficheros, quizás sea necesario más adelante.
+ * 
+ *      public async void abrirArchivo()
+        {
+            var openPicker = new FileOpenPicker();  // Nos permite abrir el explorador de ficheros
+            fotos.Add(await openPicker.PickSingleFileAsync()); //Añade la foto elegida a nuestra lista de fotos desde el explorador.
+        }
+ * 
+ */
--- 11 - Solarizr/11 - Solarizr/Views/Pedidos.xaml.cs
+++ 11 - Solarizr/11 - Solarizr/Views/Pedidos.xaml.cs
@@ -22,9 +22,40 @@ namespace _11___Solarizr.Views
     /// </summary>
     public sealed partial class Pedidos : Page
     {
+        public class contenidoLista
+        {
+            public string fecha { get; set; }
+            public string nombre { get; set; }
+            public string apellidos { get; set; }
+            public string direccion { get; set; }
+        }
+
+        public List<contenidoLista> ContentList = new List<contenidoLista>();
+
         public Pedidos()
         {
             this.InitializeComponent();
+
+            contenidoLista cita1 = new contenidoLista()
+            {
+                nombre = "Oscar",
+                apellidos = "Funes Trigo",
+                fecha = "23/10/2018",
+                direccion = "c/Falsa"
+            };
+
+            ContentList.Add(cita1);
+            contenidoLista cita2 = new contenidoLista()
+            {
+                nombre = "Sefuran-kun",
+                apellidos = "Flowered Flowers",
+                fecha = "21/10/2018",
+                direccion = "c/NoHetero"
+            };
+            ContentList.Add(cita2);
+            ContentList.Add(cita1);
+
+            lsvCitas.DataContext = ContentList;
         }
     }
 }
