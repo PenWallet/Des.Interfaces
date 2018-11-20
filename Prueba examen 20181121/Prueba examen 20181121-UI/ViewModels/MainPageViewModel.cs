6f382764c833ab06e5ea05da5c798730f9ff2ae8
Unknown <carterasindinero@gmail.com>
2018-11-20 14:37:46 +0100

    Examen y prueba de examen

---
+++ Prueba examen 20181121/Prueba examen 20181121-UI/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModels
+{
+    /// <summary>
+    /// Este es el ViewModel que el MainPage necesitará, que es lo siguiente:
+    /// 
+    ///     - ListadoCategorías: Consultable, modificable
+    ///     - ListadoPersonajes: Consultable, modificable
+    ///     - CampeónSeleccionado: Consultable, modificable
+    ///     
+    /// </summary>
+    class MainPageViewModel
+    {
+    }
+}
